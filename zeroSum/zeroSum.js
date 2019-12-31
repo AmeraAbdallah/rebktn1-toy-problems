dd1614315b3788a64002331dfa622d2b7302a5ad
Amera Ayad <amera.ayad.it@gmail.com>
2019-12-31 09:15:07 +0100

    solved

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,27 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+function zeroSum(arr){
+    let col = {};
+    for(let i = 0; i < arr.length; i++){ // O(n)
+        if(arr[i] < 0){
+            if(col[arr[i] * -1] !== undefined){
+                col[arr[i] * -1] = arr[i] 
+            } else if(col[arr[i]] === undefined){
+                col[arr[i]] = 0; 
+            } 
+        } else {
+            if(col[arr[i] * -1] !== undefined){
+                col[arr[i] * -1] = arr[i]; 
+            } else if(col[arr[i]] === undefined){
+                col[arr[i]] = 0;
+            } 
+        }
+    }
+    for(let key in col){ // O(n)
+        if(!key + col[key]) return true;
+    }
+    return false; // O(n) + O(n) = O(n)
+}
+
