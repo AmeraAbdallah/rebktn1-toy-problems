68140e3a16a67020ccac25122f656b325a7e3712
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-03 09:32:45 +0100

    solved

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,24 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string, result = {}, i = 0, next= 0,  current = string[0]) {
+    if(string.length === 0){
+        let max = [0, 0];
+        for(let key in result){
+            if(result[key][1] - result[key][0] > max[1] - max[0]){
+                max = result[key];
+            }
+        }
+        return max;
+    }
+    string = string.substr(1);
+    if(result[current+next] && current === string[0]){
+        result[current+next][1] = i+1;
+    } else if(current === string[0]){
+        result[current+next] = [i, i+1]
+    }
+    if(current !== string[1]){
+        next++;
+    }
+    return longestRun(string, result, ++i, next);
+}
