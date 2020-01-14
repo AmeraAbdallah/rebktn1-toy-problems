9932833a860ae3f7656095b662220fe29d0fd87c
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-14 10:02:02 +0100

    solved

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,33 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+function findDeepestLevel(array, level = 2) {
+    if(array.length === 1 && !Array.isArray(array[0])){
+        return level;
+    }
+
+    if(array.length === 1 && !Array.isArray(array[0]) && !array[0]){
+        return 0;
+    }
+
+    for(let i = 0; i < array.length; i++){
+        if(Array.isArray(array[i])){
+            level = level + 1;
+            return findDeepestLevel(array[i], level);
+        }
+    }
+}
+
+function arrayCaption(array){
+    let result = [0];
+    for(var i = 0; i < array.length; i++){
+        if(Array.isArray(array[i])){
+            let levelDeep = findDeepestLevel(array[i]);
+            if(levelDeep){
+                result.push(levelDeep);
+            }
+        }
+    }
+    return Math.max(...result);
+}
