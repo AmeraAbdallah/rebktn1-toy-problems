c9ed4b5f4c3641dc67a137608643c02f637f9db5
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-27 10:08:43 +0100

    later

--- numberToEnglish/numberToEnglish.js
+++ numberToEnglish/numberToEnglish.js
@@ -22,6 +22,7 @@ var numbersToWords = {
   7: 'seven',
   8: 'eight',
   9: 'nine',
+
   10: 'ten',
   11: 'eleven',
   12: 'twelve',
@@ -42,17 +43,21 @@ var numbersToWords = {
   90: 'ninety'
 };
 
-var numbersToPlace = {
-  10: 'ten',
-  100: 'hundred',
-  1000: 'thousand',
-  1000000: 'million',
-  1000000000: 'billion',
-  1000000000000: 'trillion',
-  1000000000000000: 'quadrillion',
-  1000000000000000000: 'quintillion'
-};
+var numbersToPlace = [
+  [10, 'ten', 2], //2
+  [100, 'hundred', 3], //3
+  [1000, 'thousand', 4], //4
+  [1000000, 'million', 7], //7
+  [1000000000, 'billion', 10], //10
+  [1000000000000, 'trillion', 13], //13
+  [1000000000000000, 'quadrillion', 16], //16
+  [1000000000000000000, 'quintillion', 19] //19
+];
 
-function numberToEnglish(number) {
-  // your code here...
+function numberToEnglish(number, numbersToPlace, result = ''){
+  let stringNumber = number.toString();
+  if(stringNumber.length === numbersToPlace[0][2]){ 
+    result += numbersToWords[stringNumber[0]] + numbersToPlace[0][1]
+  }
+  return result;
 }
