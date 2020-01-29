8c62704b3770b472a4ea9466858859432d3c6ecc
Amera Ayad <amera.ayad.it@gmail.com>
2020-01-29 10:12:50 +0100

    not working correctly

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,31 @@ Other palindromes in the above string include "dad", "ete", " dad " (including w
 */
 
 function longestPalindrome(string) {
-  // your code here...
+  let palindrome = [];
+  let counter = 1;
+  while(counter < string.length){
+    let loopLength = (counter < (string.length - counter)) ? counter : (string.length - counter); 
+    console.log(loopLength, 'bbbbbb')
+    let palindromeItem = '';
+    console.log(palindromeItem, 1)
+    for(let i = 1; i <= loopLength; i++){
+      if(string[counter - i] === string[counter + i]){
+        palindromeItem = string[counter + i] + palindromeItem;
+        console.log(palindromeItem, 2)
+      } else {
+        let reversed = palindromeItem.split('').reverse().join('');
+        console.log(palindromeItem, reversed, 'oooooo');
+        palindromeItem += reversed;
+  
+        palindrome.push(palindromeItem);
+        console.log(palindromeItem, 3);
+        palindromeItem = "";
+        ++counter;
+      }
+      console.log(palindromeItem, 4)
+    }
+    ++counter;
+  }
+  console.log(palindrome)
+  return palindrome.sort(function (a, b) { return b.length - a.length; })[0];
 }
