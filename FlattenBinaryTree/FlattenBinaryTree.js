252e1b61afc6ca44c3a5fdf7f99b7d52ba2cab42
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-01 09:52:54 +0100

    solved

--- FlattenBinaryTree/FlattenBinaryTree.js
+++ FlattenBinaryTree/FlattenBinaryTree.js
@@ -35,6 +35,48 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
-var flatten = function (root) {
 
+ // Node class 
+class Tree { 
+    constructor(value) { 
+        this.value = value; 
+        this.left = null; 
+        this.right = null;
+    } 
+
+    map = (cb) => {
+        cb(this.value);
+        if(this.right){
+            this.right.map(cb);
+        }
+        if(this.left){
+            this.left.map(cb);
+        }
+    }
+} 
+
+let root = new Tree(1);
+root.left = new Tree(2);
+root.left.left = new Tree(3);
+root.left.right = new Tree(4);
+root.right = new Tree(5);
+root.right.right = new Tree(6);
+
+let flatten = function (root) {
+    let array = [];
+    root.map((value) => array.push(value));
+    array = array.sort((a, b) => a - b);
+    let fromArrayToFlattenTree = (root, array) => {
+        array = array.slice(1);
+        if(array.length === 0) return;
+        root.right = new Tree(array[0]);
+        root.left = null;
+        fromArrayToFlattenTree(root.right, array);
+    }
+    root.value = array[0];
+    fromArrayToFlattenTree(root, array);
 };
+
+flatten(root);
+
+root;
