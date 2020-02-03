070eb614e115dca95c93a66d27a491b76a91a3a1
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-03 09:35:23 +0100

    solved

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -3,6 +3,33 @@ Sum of Primes
 Given a positive number, return the sum of all positive primes that are less than or equal to that number.
 */
 
+function primeTester(n) {
+    if(n === 1) return false;
+    let numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+
+    for(let i = 0; i < numbers.length; i++){
+        if(n !== numbers[i]) {
+            if(Math.floor(n / numbers[i]) === n/numbers[i]){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 function sumOfPrimes(n) {
-    // your code here...
+    if( n < 0) return 'number is negative !';
+    if( n === 0 ) return 'unvalid number !'; 
+    let sumOfPrimes = 0;
+    for(let i = 1; i <= n; i++){
+        if(primeTester(i)){
+            if(sumOfPrimes+i <= n){
+                sumOfPrimes = sumOfPrimes + i;
+            }
+            else if(sumOfPrimes+i > n){
+                return sumOfPrimes;
+            }
+        }
+    }
 }
