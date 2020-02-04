ba8a807daae4e5a9c2f4ddb8a3d800a02b156956
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-04 10:31:01 +0100

    miising lots of test cases >> later

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -19,8 +19,39 @@ In other words, find all the possible combinations of coins that sum to a given 
 */
 
 // HELPERS
-var coins = [1, 2, 5, 10, 20, 50, 100, 200];
+let coins = [1, 2, 5, 10, 20, 50, 100, 200];
+
+function multiplyPossiblities(total, coin, result){
+  let possibility1 = total / coin;
+  if(possibility1/ parseInt(possibility1) === 1 && total !== coin){
+    result.push(`${possibility1} * ${coin}p`);
+  }
+}
+
+function getSumPossiblities(number, coins, result = []){
+  if(coins.length === 0) return result;
+  if(number - coins[coins.length-1] >= 0) {
+    result.push(number - coins[coins.length-1]);
+  }
+  for(let i = coins.length - 1; i >= 0; i--){
+    //do recursion here
+  }
+}
 
 function coinSums(total) {
-  // your code here...
+  if (total <= 0) return 'please enter valid number';
+  let possibleCoins = [];
+  let result = [];
+
+  for(let i = 0; i < coins.length; i++){
+    if(coins[i] <= total){
+      possibleCoins.push(coins[i]);
+    } else break;
+  }
+
+  for(let i = 0; i < possibleCoins.length; i++){
+    multiplyPossiblities(total, possibleCoins[i], result);
+  }
+  
+  return result;
 }
