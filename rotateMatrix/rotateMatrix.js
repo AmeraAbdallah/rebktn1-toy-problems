3701f57c57a8b5fd58a82db3a9e54d2eee18ea12
Amera Ayad <amera.ayad.it@gmail.com>
2020-02-20 09:09:26 +0100

    solved

--- rotateMatrix/rotateMatrix.js
+++ rotateMatrix/rotateMatrix.js
@@ -2,8 +2,8 @@
 Write a function that rotates a NxN matrix 90 degrees clockwise. A matrix, also called a 2-D array, is simply an array of arrays of values.
 2x2 MATRIX EXAMPLE:
 [ 
-  [1, 2], 
-  [3, 4] 
+  [1, 2],  0,0 0,1
+  [3, 4]   1,0 1,1
 ]
 Rotated:
 [ 
@@ -33,5 +33,45 @@ This usually matches the way arrays are addressed in code, but keep in mind that
 */
 
 function rotateMatrix(matrix) {
-  // your code here...
+  let counter = matrix.length - 1;
+  let result = [];
+
+  for(let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for(let j = 0; j < matrix.length; j++) {
+      // i = j, j = length - i - 1
+      result[i].push(matrix[matrix.length-1-j][i])
+      // result[j][matrix.length - 1 - i] = matrix[i][j];
+    }
+  }
+  return result;
 }
+
+rotateMatrix([ 
+  ["D", 9, 5, 1],          
+  ["E", "A", 6, 2], 
+  ["F","B", 7, 3], 
+  ["G","C", 8, 4] 
+]);
+
+/* i,j i,j
+ * 0,0 0,3  i = j, j = length - i - 1
+ * 0,1 1,3
+ * 0,2 2,3
+ * 0,3 3,3
+ */
+
+ /*
+ * 1,0 0,2
+ * 1,1 1,2
+ * 1,2 2,2
+ * 1,3 3,2
+ */
+
+  /*
+   i,j i,j
+ * 3,0 0,0  i =j, j = length-1-i
+ * 3,1 1,0
+ * 3,2 2,0
+ * 3,3 3,0
+ */
